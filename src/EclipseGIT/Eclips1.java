16c7b1c2425c2d1b046bf34af9f2cdcd570fffd7
JuanManuelGarcíaDíaz <JuanManuelGarcaDaz@CSMD3PC00089-CA>
2023-03-08 19:26:10 +0100

    primer commit desde rama2

--- src/EclipseGIT/Eclips1.java
+++ src/EclipseGIT/Eclips1.java
@@ -5,7 +5,7 @@ public class Eclips1 {
 	public static void main(String[] args) {
 	System.out.println("Primer comando Eclipse GIT");
 	System.out.println("Segundo comando Eclipse GIT");
-
+	System.out.println("Primer comando desde rama2");
 	}
 
 }
